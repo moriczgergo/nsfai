51843c9cad22cb9d96cec167959402eef4b9524a
Gergő Móricz <mo.geryy@gmail.com>
2018-04-13 17:54:26 +0200

    Added mimetype-related tests.

--- test/test.js
+++ test/test.js
@@ -14,6 +14,14 @@ describe("dataParser", function() {
         expect(testResult).to.be.an("Object").and.to.haveOwnProperty("base64", testBase64);
     });
 
+    it("should filter non-supported Base64 Data URLs", function() {
+        var testBase64 = "aGVsbG8hIHRoYW5rcyBmb3IgcHV0dGluZyBpbiBlZmZvcnQgdG8gZGVjb2RpbmcgdGhpcywgYnV0IHlvdSdyZSB3YXN0aW5nIHRpbWUuLi4gZ290dGEgYmUgcHJvZHVjdGl2ZSEgOikgOykgKGFsc28gSSBrbm93IHRoaXMgaXMgbm90IGEgcmVhbCBpbWFnZSwgSSBkaWRuJ3QgbWVhbiBpdCB0byBiZSBvbmUuKQ==";
+        var dataURL = `data:image/gif;base64,${testBase64}`;
+
+        var testFunct = function() { return dataParser(dataURL); };
+        expect(testFunct).to.throw(TypeError);
+    });
+
     it("should process Base64", function() {
         var testBase64 = "aGVsbG8hIHRoYW5rcyBmb3IgcHV0dGluZyBpbiBlZmZvcnQgdG8gZGVjb2RpbmcgdGhpcywgYnV0IHlvdSdyZSB3YXN0aW5nIHRpbWUuLi4gZ290dGEgYmUgcHJvZHVjdGl2ZSEgOikgOykgKGFsc28gSSBrbm93IHRoaXMgaXMgbm90IGEgcmVhbCBpbWFnZSwgSSBkaWRuJ3QgbWVhbiBpdCB0byBiZSBvbmUuKQ==";
 
