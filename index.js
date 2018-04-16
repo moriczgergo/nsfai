174895c0abd18e21bbcfc08c3d8859fe7304f3e9
Gergő Móricz <mo.geryy@gmail.com>
2018-04-16 20:20:04 +0200

    Added better video support: returns an array of results for every second of the video.

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ var dataParser = require("./lib/dataparser");
  */
 
 /**
- * @callback nsfai~predictCallback
- * @param {Error} error The error that happened while predicting.
- * @param {Prediction} result The prediction.
+ * @promise PredictPromise
+ * @fulfill {(Prediction|Prediction[])} The prediction's result.
+ * @reject {Object} The error.
  */
 
 class nsfai {
@@ -33,6 +33,7 @@ class nsfai {
      * Predict an image's NSFWness.
      * @param {string} data Your URL/Data URL/Base64 string.
      * @param {PredictOptions} options Prediction options.
+     * @returns {PredictPromise}
      */
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -43,17 +44,41 @@ class nsfai {
         var app = this.app;
         return new Promise(function(resolve, reject) {
             try {
+<<<<<<< HEAD
                 app.models.predict(Clarifai.NSFW_MODEL, dataParser(data)).then(
+=======
+                if (!options) {
+                    options = {};
+                }
+                var _options = Object.assign({
+                    video: false
+                }, options);
+                var dataObject = dataParser(data);
+                var isVideo = options.video || dataObject.video;
+                app.models.predict(Clarifai.NSFW_MODEL, dataObject, { video: isVideo }).then(
+>>>>>>> 8dd05e8 (Added better video support: returns an array of results for every second of the video.)
                     function(response) {
-                        resolve({
-                            sfw: response.outputs[0].data.concepts[0].name === "sfw",
-                            confidence: response.outputs[0].data.concepts[0].value // confidence (0-1) about the result
-                        });
+                        if (isVideo) {
+                            var resultArray = response.outputs[0].data.frames.map(function(x) {
+                                return {
+                                    sfw: x.data.concepts[0].name === "sfw",
+                                    confidence: x.data.concepts[0].value
+                                }
+                            });
+                            resolve(resultArray);
+                        } else {
+                            resolve({
+                                sfw: response.outputs[0].data.concepts[0].name === "sfw",
+                                confidence: response.outputs[0].data.concepts[0].value // confidence (0-1) about the result
+                            });
+                        }
                     },
                     function (err) {
                         reject(err);
                     }
-                );
+                ).catch(function (err) {
+                    reject(err);
+                });
             } catch(err) {
                 reject(err);
             }
