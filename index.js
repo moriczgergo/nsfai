0fb65211f151d2c8f76f18ae1347dc553ac75c4d
Gergő Móricz <mo.geryy@gmail.com>
2023-10-28 16:49:34 +0200

    fix merge

--- index.js
+++ index.js
@@ -35,18 +35,11 @@ class nsfai {
      * @param {PredictOptions} options Prediction options.
      * @returns {PredictPromise}
      */
-<<<<<<< HEAD
-<<<<<<< HEAD
-    predict(data, cb) {
-=======
     predict(data, options) {
->>>>>>> 2e4d0c0 (Removed the cb argument from predict. 🤦)
         var app = this.app;
         return new Promise(function(resolve, reject) {
             try {
-<<<<<<< HEAD
                 app.models.predict(Clarifai.NSFW_MODEL, dataParser(data)).then(
-=======
                 if (!options) {
                     options = {};
                 }
@@ -56,7 +49,6 @@ class nsfai {
                 var dataObject = dataParser(data);
                 var isVideo = options.video || dataObject.video;
                 app.models.predict(Clarifai.NSFW_MODEL, dataObject, { video: isVideo }).then(
->>>>>>> 8dd05e8 (Added better video support: returns an array of results for every second of the video.)
                     function(response) {
                         if (isVideo) {
                             var resultArray = response.outputs[0].data.frames.map(function(x) {
@@ -83,31 +75,6 @@ class nsfai {
                 reject(err);
             }
         });
-=======
-    predict(data, cb, options) {
-        try {
-            if (!options) {
-                options = {};
-            }
-            var _options = Object.assign({
-                video: false
-            }, options);
-            var dataObject = dataParser(data);
-            this.app.models.predict(Clarifai.NSFW_MODEL, dataObject, { video: options.video || dataObject.video }).then(
-                function(response) {
-                    cb(null, {
-                        sfw: response.outputs[0].data.concepts[0].name === "sfw",
-                        confidence: response.outputs[0].data.concepts[0].value // confidence (0-1) about the result
-                    });
-                },
-                function (err) {
-                    cb(err, null);
-                }
-            );
-        } catch(err) {
-            cb(err, null);
-        }
->>>>>>> 467aca1 (Added video support to predict().)
     }
 }
 
